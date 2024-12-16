ec6bae0c4f6722d6dc98d4f9f923ae771dca4ad4
Steft91 <stefany152005@gmail.com>
2024-12-16 09:19:01 -0500

    Scripting

---
+++ Parcial2/Taller 1 - Introducción JS/js/first.js
@@ -0,0 +1,25 @@
+var nombre;
+var edad;
+var altura;
+
+nombre = "Juan";
+edad = 20;
+altura = 1.80;
+
+console.log("El nombre de la persona es:" + nombre);
+console.log("La edad de la persona es: " + edad);
+console.log("La altura de " + nombre + " es " + altura );
+
+//Var para declarar una variable global
+//let para declarar una variable local o tambien una variable de bloque
+//const declara una variable constante es decir que su valor no va a cambiar
+
+
+//if marca un bloque de instrucciones que se ejecutan si una condicion se cumple
+//switch marca un bloque de instrucciones que se ejecutan si una condicion se cumple
+//for marca una condicioon de istrucciones que se ejecutan varias veces
+//while marca una ocndicion de instrucciones que se ejecutan varias veces
+
+//function marca un bloque de instrucciones que se ejecutan cuando se llama
+//try se ejecuta cuando no se produce un error
+//return se ejecuta cuando una con
